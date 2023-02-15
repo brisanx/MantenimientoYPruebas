4a45cfbbc50ffa166985e8d3814b90358b5e51d9
brisanx <albasanchezibanez6@uma.es>
2023-02-15 21:13:29 +0100

    Just checking

--- src/test/java/org/mps/person/PersonTest.java
+++ src/test/java/org/mps/person/PersonTest.java
@@ -13,51 +13,87 @@ import static org.junit.jupiter.api.Assertions.*;
  * @author Alba Sánchez Ibáñez
  *
  * Test cases:
- * 1. Age is not valid, as it can't be negative, otherwise it throws RuntimeException
- * 2. Gender must be Male or Female, otherwise it throws RuntimeException
- * 3. All getters should return its respective value
+ * 1. Age is not valid, as it can't be negative, otherwise it throws NegativeAgeException
+ * 2. Gender must be Male or Female, otherwise it throws WrongGenderException
+ * 3. All getters should return its respective value (These are: name, age and gender)
  * 4. Valid case for the method averageAgePerGender,
  *    for some specific valid people it returns correctly the average age per gender
+ *   5. Empty list will return an EmptyListException
+ *   6. Given a list with only men, the mean age for women will be 0, same thing happens with only women
  */
 class PersonTest {
     Person person;
+    Person person2;
+    List<Person> persons;
+
     @BeforeEach
-    void setup(){
+    void setup() throws NegativeAgeException, WrongGenderException {
+        persons = new ArrayList<>();
         person = new Person("Alba",19,"Female");
+        person2 = new Person("Juan", 42, "Male");
     }
     @AfterEach
     void shutdown(){
         person = null;
+        person2 = null;
+        persons = null;
     }
 
     @Test
-    void notValidGenderShouldThrowRunTimeException(){
-        assertThrows(RuntimeException.class, ()->new Person("Alba", 19, "Patata"));
+    void notValidGenderShouldThrowWrongGenderException(){
+        assertThrows(WrongGenderException.class, ()->new Person("Alba", 19, "Patata"));
     }
    @Test
-    void notValidAgeShouldThrowRunTimeException(){
-        assertThrows(RuntimeException.class, ()->new Person("Alba", -2, "Female"));
+    void notValidAgeShouldThrowNegativeAgeException(){
+        assertThrows(NegativeAgeException.class, ()->new Person("Alba", -2, "Female"));
     }
     @Test
     void validGetterForAgeReturnAge(){
-        assertEquals(person.age(),19);
+        int actualAge = person.age();
+        int expectedAge = 19;
+        assertEquals(expectedAge,actualAge);
     }
     @Test
     void validGetterForNameReturnName(){
-        assertEquals(person.name(), "Alba");
+        String actualName = person.name();
+        String expectedName = "Alba";
+        assertEquals(expectedName,actualName);
     }
     @Test
     void validGetterForGenderReturnGender(){
-        assertEquals(person.gender(),"Female");
+        String actualGenderFemale = person.gender().toLowerCase();
+        String expectedGenderFemale = "female";
+        assertEquals(actualGenderFemale,expectedGenderFemale);
+    }
+
+    @Test
+    void validAverageAgePerGender() throws NegativeAgeException, WrongGenderException, EmptyListException {
+        persons.add(new Person("Pedro", 20,"Male"));
+        persons.add(new Person("Marta", 40,"female"));
+        persons.add(new Person("Javier", 60,"male"));
+        persons.add(new Person("Lucia", 30,"Female"));
+
+        double[] expectedValues = {40,35};
+        double[] actualValues = person.averageAgePerGender(persons);
+
+        assertEquals(expectedValues[0],actualValues[0]);
+        assertEquals(expectedValues[1],actualValues[1]);
     }
     @Test
-    void validAverageAgePerGender(){
-        List<Person> persons = new ArrayList<>();
+    void zeroFemaleInTheListShouldReturn() throws NegativeAgeException, WrongGenderException, EmptyListException {
         persons.add(new Person("Pedro", 20,"Male"));
-        persons.add(new Person("Marta", 40,"Female"));
-        persons.add(new Person("Javier", 60,"Male"));
-        persons.add(new Person("Lucia", 30,"Female"));
-        assertEquals(person.averageAgePerGender(persons)[0],40);
-        assertEquals(person.averageAgePerGender(persons)[1],35);
+        persons.add(new Person("Javier", 60,"male"));
+        assertEquals(person.averageAgePerGender(persons)[1],0.0);
     }
+    @Test
+    void zeroMaleInTheListShouldReturn() throws NegativeAgeException, WrongGenderException, EmptyListException {
+        persons.add(new Person("Pedra", 20,"Female"));
+        persons.add(new Person("Javiera", 60,"female"));
+        assertEquals(person.averageAgePerGender(persons)[0],0.0);
+    }
+    @Test
+    void emptyListReturnsEmptyError() {
+        assertThrows(EmptyListException.class, ()->person.averageAgePerGender(persons));
+    }
+
 }
